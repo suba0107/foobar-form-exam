2ecd7c7d95ce0428e1f7808cbd58f07e997445d4
rachelvo <rach0727@stud.kea.dk>
2020-05-20 15:54:38 +0200

    added MUI

--- form-react/src/components/ButtonBack.js
+++ form-react/src/components/ButtonBack.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
+import Button from "muicss/lib/react/button";
 
 export default function ButtonBack(props) {
-  return <button>Back</button>;
+  return <Button size="small" variant="fab"></Button>;
 }
