d8e99bd4286b984db3ec5a7a651ad766d5edcf5a
rachelvo <rach0727@stud.kea.dk>
2020-05-21 13:03:58 +0200

    done styling back btn

--- form-react/src/components/ButtonBack.js
+++ form-react/src/components/ButtonBack.js
@@ -1,6 +1,28 @@
 import React, { useState } from "react";
+import { render } from "react-dom";
+import { ReactSVG } from "react-svg";
 import Button from "muicss/lib/react/button";
+import styles from "./ButtonBack.module.css";
+import BackLogo from "../svgs/back_logo.svg";
 
 export default function ButtonBack(props) {
-  return <Button size="small" variant="fab"></Button>;
+  return (
+    <Button className={styles.roundBackBtn} size="small" variant="fab">
+      <ReactSVG
+        src={BackLogo}
+        afterInjection={(error, svg) => {
+          if (error) {
+            console.error(error);
+            return;
+          }
+          // console.log(svg);
+        }}
+        beforeInjection={(svg) => {
+          svg.setAttribute("style", "width: 6.5vw");
+          svg.style.position = "relative";
+          svg.style.left = "-2vw";
+        }}
+      />
+    </Button>
+  );
 }
