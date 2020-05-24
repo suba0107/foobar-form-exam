eb59e9917c330584f45be01dd52834677d336d40
rachelvo <rach0727@stud.kea.dk>
2020-05-24 18:32:38 +0200

    done media queries for desktop (macbook pro)

--- form-react/src/components/PaymentIntro.js
+++ form-react/src/components/PaymentIntro.js
@@ -4,8 +4,10 @@ import styles from "./PaymentIntro.module.css";
 export default function PaymentIntro(props) {
   return (
     <article className={styles.paymentIntro}>
-      <h2>Almost there!</h2>
-      <h2>Please choose a payment method</h2>
+      <div>
+        <h2>Almost there!</h2>
+        <h2>Please choose a payment method</h2>
+      </div>
       {props.children}
     </article>
   );
