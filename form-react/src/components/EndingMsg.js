a45027128686cbf773bf1ed93e6915c2e98dd8b5
rachelvo <rach0727@stud.kea.dk>
2020-06-03 15:55:24 +0200

    correct setInterval for endingScr

--- form-react/src/components/EndingMsg.js
+++ form-react/src/components/EndingMsg.js
@@ -4,51 +4,25 @@ import Checkmark from "../svgs/checkmark_status.svg";
 import styles from "./EndingStatusBox.module.css";
 
 export default function EndingMsg(props) {
-  const statusMsg = useRef("");
-  useEffect(() => {
-    const msg2 = setInterval(() => {
-      statusMsg.current.textContent = "A bartender has accepted your order.";
-      console.log(msg2);
-    }, 4000);
-    return () => {
-      clearInterval(msg2);
-    };
-  }, []);
-  useEffect(() => {
-    const msg3 = setInterval(() => {
-      statusMsg.current.textContent = "Pouring beer...";
-    }, 7000);
-    return () => {
-      clearInterval(msg3);
-    };
-  }, []);
-  useEffect(() => {
-    const msg4 = setInterval(() => {
-      statusMsg.current.textContent = "Enjoy your beer!";
-    }, 10000);
-    return () => {
-      clearInterval(msg4);
-    };
-  }, []);
-  // const statusMsg = setInterval(changeStatus, 4000);
-  // function changeStatus() {
-  //   document.querySelector("#status-msg").textContent =
-  //     "A bartender has accepted your order.";
-  //   return clearInterval(statusMsg);
-  // }
-  // const statusMsg2 = setInterval(changeStatus2, 7000);
-  // function changeStatus2() {
-  //   document.querySelector("#status-msg").textContent = "Pouring beer...";
-  //   return clearInterval(statusMsg2);
-  // }
-  // const statusMsg3 = setInterval(changeStatus3, 10000);
-  // function changeStatus3() {
-  //   document.querySelector("#status-msg").textContent = "Enjoy your beer!";
-  //   return clearInterval(statusMsg3);
-  // }
+  const status = useRef("");
+  const statusMsg = setInterval(changeStatus, 4000);
+  function changeStatus() {
+    status.current.textContent = "A bartender has accepted your order.";
+    return clearInterval(statusMsg);
+  }
+  const statusMsg2 = setInterval(changeStatus2, 7000);
+  function changeStatus2() {
+    status.current.textContent = "Pouring beer...";
+    return clearInterval(statusMsg2);
+  }
+  const statusMsg3 = setInterval(changeStatus3, 10000);
+  function changeStatus3() {
+    status.current.textContent = "Enjoy your beer!";
+    return clearInterval(statusMsg3);
+  }
 
   return (
-    <h3 id="status-msg" ref={statusMsg}>
+    <h3 id="status-msg" ref={status}>
       Waiting for a bartender to accept your order.
     </h3>
   );
