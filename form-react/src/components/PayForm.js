b3428378d1752941df2673bd3ac14997066e44f9
rachelvo <rach0727@stud.kea.dk>
2020-06-03 14:52:32 +0200

    added error msgs and can't fix the CVV no.

--- form-react/src/components/PayForm.js
+++ form-react/src/components/PayForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ButtonPay from "./ButtonPay";
 import { Heroku } from "../modules/Heroku";
 import NumberFormat from "react-number-format";
@@ -7,32 +7,19 @@ import MastercardIcon from "../images/mastercard.png";
 import VisaIcon from "../images/visa.png";
 import styles from "./PayForm.module.css";
 import { useHistory } from "react-router-dom";
-import CreditCardInput from "react-credit-card-input";
 
 export default function Form(props) {
   console.log(props.orders);
-  const [orders, setOrders] = useState([]);
-  const [egname, setEgName] = useState("");
-  const [egcc, setEgCC] = useState("");
-  const [egexdate, setEgEx] = useState("");
-  const [egcvv, setEgCVV] = useState("");
-  const [text, setText] = useState({});
-  const onlyText = (evt) => {
-    let value = evt.target.value;
-    value = value.replace(/[^A-Za-z]/gi, "");
-    setText({ value });
-  };
-  // console.log(orders[1].name);
-  // const changeArr = Object.values(orders[1]); //["1", "Fairy tale"]
-  // changeArr.shift();
-  // console.log(changeArr);
   let history = useHistory();
-
+  const [orders, setOrders] = useState([]);
   useEffect(() => {
     const ordertest = [];
     props.orders.forEach((elm) => {
       if (elm.name !== undefined) {
-        ordertest.push({ name: elm.name, amount: elm.count });
+        ordertest.push({
+          name: elm.name,
+          amount: elm.count,
+        });
       }
     });
 
@@ -40,21 +27,45 @@ export default function Form(props) {
     setOrders(ordertest);
   }, [props.orders]);
 
+  const cardNoMsg = useRef("");
+  const cvvMsg = useRef("");
+  const expireDateMsg = useRef("");
+  const nameMsg = useRef("");
+  const cardInput = useRef("");
   function submit(evt) {
     evt.preventDefault();
+    // let validForm = true;
+    // const form = document.querySelector("form");
     let cardnoInput = document.querySelector(
       "#cardnumber > input:nth-child(1)"
     );
-    let value = cardnoInput.value.split(" ");
-    let trueLength = value.join("");
+    let ccvalue = cardnoInput.value.split(" ");
+    const ccLength = ccvalue.join("");
+
+    let dateInput = document.querySelector("#dateYear > input:nth-child(1)");
+    let datevalue = dateInput.value.split("/");
+    const dateLength = datevalue.join("");
+
+    let cvvInput = document.querySelector("#cvvNumber > input:nth-child(1)");
+    // let cvvLength = cvvInput.value;
     // let visa = /^4[0-9]{12}(?:[0-9]{3})?$/;
     // let master = /^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/;
-    if (trueLength.length < 16) {
+
+    if (ccLength.length < 16) {
       evt.preventDefault();
-      console.log(false);
-      document.querySelector("#cardNoExample").textContent =
-        "Invalid card number";
-    } else {
+      cardNoMsg.current.textContent = "Card number has to have 16 digits";
+      cardNoMsg.current.style.color = "var(--pink-highlight)";
+    } else if (dateLength.length < 4) {
+      evt.preventDefault();
+      expireDateMsg.current.textContent = "Expiry date's format is MM/YY";
+      expireDateMsg.current.style.color = "var(--pink-highlight)";
+    }
+    // else if (cvvLength.length < 2) {
+    //   evt.preventDefault();
+    //   cvvMsg.current.textContent = "CVV number must have 3 digits";
+    //   cvvMsg.current.style.color = "var(--pink-highlight)";
+    // }
+    else {
       Heroku.postOrder(orders);
       history.push("/end");
     }
@@ -82,17 +93,7 @@ export default function Form(props) {
 
     return month + (year.length ? "/" + year : "");
   }
-  function hideExample(evt) {
-    document
-      .querySelectorAll("#cardNoExample,#expireDateExample,#cvvExample")
-      .forEach((elm) => {
-        elm.textContent = "";
-      });
-  }
-  function checkName(evt) {
-    // let value = evt.target.value.replace(/[^A-Za-z]/, "");
-    // //value = value.replace(/[^A-Za-z]/, "");
-    // setState(value);
+  const onlyTextAllow = (evt) => {
     //https://stackoverflow.com/questions/29823591/html-input-do-not-allow-numbers
     let key = evt.keyCode;
     if (
@@ -106,36 +107,55 @@ export default function Form(props) {
     ) {
       evt.preventDefault();
     }
-  }
 
-  // class NameCard extends React.PureComponent {
-  //   state = {
-  //     value: "",
-  //   };
-  //   render() {
-  //     return (
-  //       <input
-  //         value={this.state.value}
-  //         placeholder={"John Smith"}
-  //         onChange={(e) => {
-  //           let value = e.target.value;
-  //           value = value.replace(/[^A-Za-z]/, "");
-  //           this.setState({
-  //             value,
-  //           });
-  //         }}
-  //         required
-  //       />
-  //     );
-  //   }
-  // }
-  // document.querySelector("#card-number").oninput = function () {
-  //   var foo = this.value.split(" ").join("");
-  //   if (foo.length > 0) {
-  //     foo = foo.match(new RegExp(".{1,4}", "g")).join(" ");
-  //   }
-  //   this.value = foo;
-  // };
+    //error msgs for name
+    nameMsg.current.textContent = "E.g. John Smith";
+    nameMsg.current.style.color = "var(--bright-purple-bg)";
+  };
+  const emptyName = () => {
+    nameMsg.current.textContent = "Missing cardholder's name";
+    nameMsg.current.style.color = "var(--pink-highlight)";
+  };
+  const hideNameEg = () => {
+    nameMsg.current.textContent = "";
+  };
+
+  //Error messages
+  const emptyCC = () => {
+    cardNoMsg.current.textContent = "Missing card number";
+    cardNoMsg.current.style.color = "var(--pink-highlight)";
+  };
+  const hideCCEg = () => {
+    cardNoMsg.current.textContent = "";
+  };
+  const showCCEg = () => {
+    cardNoMsg.current.textContent = "E.g. 1234 1234 1234 1234";
+    cardNoMsg.current.style.color = "var(--bright-purple-bg)";
+  };
+
+  const showDateEg = () => {
+    expireDateMsg.current.textContent = "E.g. MM/YY";
+    expireDateMsg.current.style.color = "var(--bright-purple-bg)";
+  };
+  const emptyDate = () => {
+    expireDateMsg.current.textContent = "Missing expiry date";
+    expireDateMsg.current.style.color = "var(--pink-highlight)";
+  };
+  const hideDateEg = () => {
+    expireDateMsg.current.textContent = "";
+  };
+
+  const emptyCVV = () => {
+    cvvMsg.current.textContent = "Missing CVV number";
+    cvvMsg.current.style.color = "var(--pink-highlight)";
+  };
+  const hideCVVEg = () => {
+    cvvMsg.current.textContent = "";
+  };
+  const showCVVEg = () => {
+    cvvMsg.current.textContent = "E.g. 123";
+    cvvMsg.current.style.color = "var(--bright-purple-bg)";
+  };
 
   return (
     <form className={styles.payForm} onSubmit={submit}>
@@ -153,76 +173,63 @@ export default function Form(props) {
           placeholder="John Something"
           autoCapitalize="words"
           name="nameOnCard"
-          // onChange={checkName}
           onKeyDown={(evt) => {
-            checkName(evt);
-            setEgName("E.g. John Smith");
+            onlyTextAllow(evt);
           }}
-          pattern="[a-zA-Z]*"
+          onBlur={hideNameEg}
+          onInvalid={emptyName}
           required
         ></input>
-        <p id="nameError">{egname}</p>
+        <p id="nameExample" ref={nameMsg}></p>
       </label>
       <label className={styles.cardNumber} id="cardnumber">
         Card number
-        {/* <input
-          id="card-number"
-          placeholder="1234 5678 9012 3456"
-          onKeyDown={showCardNoExample}
-          name="card"
-          maxLength={18}
-          minLength={18}
-          required
-        /> */}
         <NumberFormat
           id="cardNo"
           name="cardNo"
           format="#### #### #### ####"
           placeholder="1234 1234 1234 1234"
-          onKeyDown={() => {
-            // showCardNoExample();
-            setEgCC("E.g. 1234 1234 1234 1234");
-          }}
-          onBlur={() => {
-            hideExample();
-          }}
-          onInput={() => {}}
+          onKeyDown={showCCEg}
+          onBlur={hideCCEg}
+          onInvalid={emptyCC}
+          // onInput={(e) => {
+          //   checkCCminLength(e);
+          // }}
+          ref={cardInput}
+          value=""
           allowEmptyFormatting={false}
           required
         />
-        <p id="cardNoExample">{egcc}</p>
+        <p id="cardNoExample" ref={cardNoMsg}></p>
       </label>
       <fieldset className={styles.fieldsetWrapper}>
-        <label className={styles.expireDateLabel}>
+        <label className={styles.expireDateLabel} id="dateYear">
           Expire
           <NumberFormat
-            id="dateYear"
             format={cardExpiry}
-            placeholder="mm/yy"
+            placeholder="MM/YY"
             className={styles.cardExpire}
-            onKeyDown={() => {
-              setEgEx("E.g. MM/YY");
-            }}
-            onBlur={hideExample}
+            onKeyDown={showDateEg}
+            onBlur={hideDateEg}
+            onInvalid={emptyDate}
+            allowEmptyFormatting={false}
             required
           />
-          <p id="expireDateExample">{egexdate}</p>
+          <p id="expireDateExample" ref={expireDateMsg}></p>
         </label>
-        <label className={styles.cvvLabel}>
+        <label className={styles.cvvLabel} id="cvvNumber">
           CVV
           <NumberFormat
-            // customInput={Input}
             format="###"
-            minLength={3}
             placeholder="123"
             className={styles.cardCVV}
-            onKeyDown={() => {
-              setEgCVV("E.g. 123");
-            }}
-            onBlur={hideExample}
+            onKeyDown={showCVVEg}
+            onBlur={hideCVVEg}
+            onInvalid={emptyCVV}
+            allowEmptyFormatting={false}
             required
           />
-          <p id="cvvExample">{egcvv}</p>
+          <p id="cvvExample" ref={cvvMsg}></p>
         </label>
       </fieldset>
       <ButtonPay type="submit"></ButtonPay>
