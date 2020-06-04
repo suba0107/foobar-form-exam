4921e35445688b9d53d6b09c7324654e056faf6e
rachelvo <rach0727@stud.kea.dk>
2020-06-04 17:58:59 +0200

    Updates

---
+++ precache-manifest.6fc02df1367432ff5e2c6bfe2ef75f53.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8d8ada21c1446b0af4396fb80f0fac",
+    "url": "/kea/3-sem/foobar-examproject/index.html"
+  },
+  {
+    "revision": "44f1db84a10db0161c8b",
+    "url": "/kea/3-sem/foobar-examproject/static/css/main.0d107205.chunk.css"
+  },
+  {
+    "revision": "1be70147436c7bcfe237",
+    "url": "/kea/3-sem/foobar-examproject/static/js/2.f6c8b5fd.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/kea/3-sem/foobar-examproject/static/js/2.f6c8b5fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44f1db84a10db0161c8b",
+    "url": "/kea/3-sem/foobar-examproject/static/js/main.c454f4f4.chunk.js"
+  },
+  {
+    "revision": "f72437311342f5a200af",
+    "url": "/kea/3-sem/foobar-examproject/static/js/runtime-main.d74770ab.js"
+  },
+  {
+    "revision": "27f7e10929e9680b3c76b649fb65f5c3",
+    "url": "/kea/3-sem/foobar-examproject/static/media/back_logo.27f7e109.svg"
+  },
+  {
+    "revision": "80a1d293953f3236226ee3edd1870dc2",
+    "url": "/kea/3-sem/foobar-examproject/static/media/checkmark_status.80a1d293.svg"
+  },
+  {
+    "revision": "f3bb1c70f470316043d97e1138b9ba55",
+    "url": "/kea/3-sem/foobar-examproject/static/media/creditcards_icon3.f3bb1c70.svg"
+  },
+  {
+    "revision": "d7c44b7e4403264b65957059ddba4edb",
+    "url": "/kea/3-sem/foobar-examproject/static/media/final-logo.3b4eeae0.png"
+  },
+  {
+    "revision": "6e330b7bd8cc0f5ec5bdbb009c47fe36",
+    "url": "/kea/3-sem/foobar-examproject/static/media/landingpage.6e330b7b.svg"
+  },
+  {
+    "revision": "1dc0c44333a43f3fb66c8654dedbe296",
+    "url": "/kea/3-sem/foobar-examproject/static/media/mastercard.e89e3fbf.png"
+  },
+  {
+    "revision": "e1dd90054d30c9dbb86f8c5f6e3b2587",
+    "url": "/kea/3-sem/foobar-examproject/static/media/mobilepay_code.e1dd9005.svg"
+  },
+  {
+    "revision": "43c4ecd24791bc627ae3a2f7f125b5ba",
+    "url": "/kea/3-sem/foobar-examproject/static/media/mobilepay_icon.43c4ecd2.svg"
+  },
+  {
+    "revision": "512608b8294ed21407bf7222df6febc2",
+    "url": "/kea/3-sem/foobar-examproject/static/media/pub-with-bartender.512608b8.svg"
+  },
+  {
+    "revision": "4446fbc493f5c7a31262f3ab7bcaffea",
+    "url": "/kea/3-sem/foobar-examproject/static/media/shopping-cart.4446fbc4.svg"
+  },
+  {
+    "revision": "e237adca44e5f3ae896ebb84bc749a78",
+    "url": "/kea/3-sem/foobar-examproject/static/media/wireless_icon.e237adca.svg"
+  },
+  {
+    "revision": "e9354bf7449aa801d3d965fb9b34f4ea",
+    "url": "/kea/3-sem/foobar-examproject/static/media/wireless_icon2.e9354bf7.svg"
+  }
+]);
